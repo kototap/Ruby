44c9c9e84b980820466cb9e2b0973e855014b452
Kototap <kotonesmz31@gmail.com>
2022-11-05 09:22:45 +0000

    lesson9

--- lesson9.rb
+++ lesson9.rb
@@ -1,48 +1,88 @@
+# インスタンスメソッド
+class Car
+  def move(direction, distance)
+    self.turn(direction)
+    self.run(distance)
+  end
+
+  def turn(direction)
+    puts "#{direction}に曲がります。"
+  end
+
+  def run(distance)
+    puts "車で#{distance}キロ走ります。"
+  end
+end
+
+car = Car.new
+car.move("右", 6)
+
+# クラスメソッド
+class Car
+  def self.run(distance)
+    puts "車で#{distance}キロ走ります。"
+  end
+end
+
+Car.run(10)
+
 
 class Car
-  def run(dis)
-    puts "#{dis}キロ走ります"
+  def self.turn(direction)
+    puts "#{direction}に曲がるヨ。"
   end
 end
 
-class Truck < Car
-  def run(dis)
-    super
-    puts "大きな荷物を乗せて走ります"
-  end
-end
-
-truck = Truck.new
-truck.run(5)
+Car.turn("right")
 
 
 
-class Flower
-  def flower(name)
-    puts "#{name}という種類のお花です。"
-  end
-end
+# class Car
+#   def run(dis)
+#     puts "#{dis}キロ走ります"
+#   end
+# end
 
-class Spring < Flower
-  def flower(name)
-    super
-    puts "春に咲きます。"
-  end
-end
+# class Truck < Car
+#   def run(dis)
+#     super
+#     puts "大きな荷物を乗せて走ります"
+#   end
+# end
 
-class Summer < Flower
-  def flower(name)
-    super
-    puts "夏に咲きます。"
-  end
-end
+# truck = Truck.new
+# truck.run(5)
 
 
-spring = Spring.new
-spring.flower("チューリップ")
 
-summer = Summer.new
-summer.flower("ひまわり")
-summer.flower("あじさい")
+
+
+# class Flower
+#   def flower(name)
+#     puts "#{name}という種類のお花です。"
+#   end
+# end
+
+# class Spring < Flower
+#   def flower(name)
+#     super
+#     puts "春に咲きます。"
+#   end
+# end
+
+# class Summer < Flower
+#   def flower(name)
+#     super
+#     puts "夏に咲きます。"
+#   end
+# end
+
+
+# spring = Spring.new
+# spring.flower("チューリップ")
+
+# summer = Summer.new
+# summer.flower("ひまわり")
+# summer.flower("あじさい")
 
 
